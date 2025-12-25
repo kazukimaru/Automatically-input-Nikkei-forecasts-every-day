5d66189052d84ac8e68fe82a07506b2c795263aa
kazukimaru <102517706+kazukimaru@users.noreply.github.com>
2025-12-25 18:10:17 +0900

    Update run.js

--- src/run.js
+++ src/run.js
@@ -1,7 +1,14 @@
-import { chromium } from "playwright";
-
 async function main() {
-  console.log("自動入力テスト成功！");
+  const url =
+    "https://query1.finance.yahoo.com/v7/finance/quote?symbols=NKD=F";
+
+  const res = await fetch(url);
+  const data = await res.json();
+
+  const price =
+    data.quoteResponse.result[0].regularMarketPrice;
+
+  console.log("📈 日経平均先物の価格:", price);
 }
 
 main();
